efe045fc8824044f5de736040e6eb2e875905c5a
ulala-x <ulala.the.great@gmail.com>
2026-02-02 13:09:20 +0900

    refactor: drop unused discovery listeners

--- src/discovery/discovery.hpp
+++ src/discovery/discovery.hpp
@@ -6,7 +6,6 @@
 #include "core/ctx.hpp"
 #include "core/thread.hpp"
 #include "utils/atomic_counter.hpp"
-#include "utils/condition_variable.hpp"
 #include "utils/mutex.hpp"
 
 #include <map>
@@ -23,22 +22,6 @@ struct provider_info_t
     zlink_routing_id_t routing_id;
     uint32_t weight;
     uint64_t registered_at;
-};
-
-enum discovery_event_t
-{
-    DISCOVERY_EVENT_PROVIDER_ADDED = 1,
-    DISCOVERY_EVENT_PROVIDER_REMOVED = 2,
-    DISCOVERY_EVENT_SERVICE_AVAILABLE = 3,
-    DISCOVERY_EVENT_SERVICE_UNAVAILABLE = 4
-};
-
-class discovery_listener_t
-{
-  public:
-    virtual ~discovery_listener_t () {}
-    virtual void on_discovery_event (int event_,
-                                     const std::string &service_name_) = 0;
 };
 
 class discovery_t
@@ -62,8 +45,6 @@ class discovery_t
 
     void snapshot_providers (const std::string &service_name_,
                              std::vector<provider_info_t> *out_);
-    void add_listener (discovery_listener_t *listener_);
-    void remove_listener (discovery_listener_t *listener_);
 
   private:
     struct service_state_t
@@ -86,10 +67,6 @@ class discovery_t
     std::map<std::string, service_state_t> _services;
     std::map<uint32_t, uint64_t> _registry_seq;
     std::set<std::string> _subscriptions;
-    std::set<discovery_listener_t *> _listeners;
-    condition_variable_t _listeners_cv;
-    size_t _listeners_in_dispatch;
-
     ZLINK_NON_COPYABLE_NOR_MOVABLE (discovery_t)
 };
 }

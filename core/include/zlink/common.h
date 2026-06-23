1e77d9caaf3dee3d98be0732dbe6451975958353
kairos-code-dev <ulalax@kairoscode.dev>
2026-06-23 12:34:41 +0900

    Release core 8.0.0

--- core/include/zlink/common.h
+++ core/include/zlink/common.h
@@ -5,10 +5,10 @@
 
 /*  Version macros for compile-time API version detection                     */
 #ifndef ZLINK_VERSION_MAJOR
-#define ZLINK_VERSION_MAJOR 7
+#define ZLINK_VERSION_MAJOR 8
 #endif
 #ifndef ZLINK_VERSION_MINOR
-#define ZLINK_VERSION_MINOR 2
+#define ZLINK_VERSION_MINOR 0
 #endif
 #ifndef ZLINK_VERSION_PATCH
 #define ZLINK_VERSION_PATCH 0

9a542fdb5b0eb2e9f3578f66fe8397254b203d77
kairos-code-dev <ulalax@kairoscode.dev>
2026-06-24 03:27:23 +0900

    java: cover discovery registry ha e2e

---
+++ framework/languages/java/e2e/DiscoveryRegistryHa/src/main/java/systems/zlink/e2e/discoveryregistryha/ProviderState.java
@@ -0,0 +1,13 @@
+package systems.zlink.e2e.discoveryregistryha;
+
+public final class ProviderState {
+    private final String providerRid;
+
+    public ProviderState(String providerRid) {
+        this.providerRid = providerRid;
+    }
+
+    public String providerRid() {
+        return providerRid;
+    }
+}

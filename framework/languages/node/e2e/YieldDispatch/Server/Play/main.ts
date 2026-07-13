4c719b301eec495b4434328e306bf80cfe4ea4f9
kairos-code-dev <ulalax@kairoscode.dev>
2026-07-13 13:54:09 +0900

    chore(node): remove superseded yield dispatch fixture

--- framework/languages/node/e2e/YieldDispatch/Server/Play/main.ts
+++
@@ -1,6 +0,0 @@
-import { startPlayHost } from './play-host-factory';
-
-startPlayHost(process.argv.slice(2)).catch((error: unknown) => {
-  console.error(error);
-  process.exitCode = 1;
-});
--- framework/languages/node/e2e/YieldDispatch/Server/Session/main.ts
+++
@@ -1,6 +0,0 @@
-import { startSessionHost } from './session-host-factory';
-
-startSessionHost(process.argv.slice(2)).catch((error: unknown) => {
-  console.error(error);
-  process.exitCode = 1;
-});

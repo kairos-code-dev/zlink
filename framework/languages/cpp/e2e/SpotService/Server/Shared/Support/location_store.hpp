920e51a0e14e7f3620ebf5d0cfa0a22dd9a178ed
kairos-code-dev <ulalax@kairoscode.dev>
2026-07-16 05:25:05 +0900

    fix(cpp): apply location options by reference

--- framework/languages/cpp/e2e/SpotService/Server/Shared/Support/location_store.hpp
+++ framework/languages/cpp/e2e/SpotService/Server/Shared/Support/location_store.hpp
@@ -23,7 +23,7 @@ inline void add_redis_location_store (zlink::framework::zlink_framework_options_
       std::make_shared<zlink::framework::locations::redis::redis_location_store_t> (
         zlink::framework::locations::redis::redis_location_options_t{
           .connection_string = redis_endpoint, .key_prefix = redis_key_prefix}));
-    auto locations = framework.configure_locations ();
+    auto &locations = framework.configure_locations ();
     locations.heartbeat_interval = std::chrono::seconds (1);
     locations.owner_lease_ttl = std::chrono::seconds (3);
     locations.polling_interval = std::chrono::milliseconds (500);
